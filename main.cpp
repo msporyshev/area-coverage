c3bd844d5eff8e5f088b7302bfc271fdb10e707b
Max Sporyshev <coockoombra@gmail.com>
2017-06-09 18:04:30 +1000

    Fix domain size

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ namespace bg = boost::geometry;
 int main(int argc, char** argv)
 {
     std::vector<Point> grid;
-    points_on_square_grid_2( 1000.0, 1000, std::back_inserter(grid),Creator());
+    points_on_square_grid_2( 1000.0, 900, std::back_inserter(grid),Creator());
 
-    Polygon domain = random_poly(15, 1000, 777);
+    Polygon domain = random_poly(15, 900, 777);
 
     SvgFrame output(1000, "output.svg");
     output.add_grid(grid).add_domain(domain);
